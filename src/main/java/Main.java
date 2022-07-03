32da427ad57f9bb8db63bbe06254d8c06abd8764
octavia <Ogomes21@gmail.com>
2022-07-03 19:36:42 -0400

    Initial code for sum of input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
+//prompt user to input a number
+//printing the sum of the numbers 1 to n
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        int n = userInput.nextInt();
+        int sum = 0;
+
+        System.out.println("Enter an integer.");
+
+        for (n = 0; n <= userInput.nextInt(); n++) {
+            sum += n;
+        }
 
     }
 }
+
